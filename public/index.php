8dac6ea30bbe09b6e28af0ffe6ac2b19718e5342
Al. Chechet <al.chechet@gmail.com>
2023-08-25 01:40:38 +0300

    Check environment

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
-<?php phpinfo();
+<a href="info.php">php info</a>
+<hr>
+<?php echo 'FOO: ', getenv('FOO'); ?>
+<hr>_ENV:
+<pre><?php print_r($_ENV)?></pre>
+<br>_SERVER:
+<pre><?php print_r($_SERVER)?></pre>
